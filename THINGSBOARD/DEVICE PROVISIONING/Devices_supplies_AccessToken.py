0e18857ebf271bee99819525c26a534cd80c7c42
michelemenzione <menzione.m@gmail.com>
2023-11-28 10:25:13 +0100

    newFeature

--- THINGSBOARD/DEVICE PROVISIONING/Devices_supplies_AccessToken.py
+++ THINGSBOARD/DEVICE PROVISIONING/Devices_supplies_AccessToken.py
@@ -1,47 +1,47 @@
 from paho.mqtt.client import Client
 from json import dumps, loads
 
-# Dizionario per associare i codici di risultato della connessione a messaggi più descrittivi
+# Definizione dei codici di risultato per la connessione MQTT
 RESULT_CODES = {
-    1: "incorrect protocol version",
-    2: "invalid client identifier",
-    3: "server unavailable",
-    4: "bad username or password",
-    5: "not authorised",
+    1: "versione di protocollo non corretta",
+    2: "identificatore del client non valido",
+    3: "server non disponibile",
+    4: "username o password non valide",
+    5: "non autorizzato",
 }
 
-# Nome del file che conterrà le credenziali
-FILE_CREDENTIALS = "credentialsGeneratedByServer"
+# Nome del file che conterrà le credenziali fornite dal dispositivo
+FILE_CREDENTIALS = "credentialsSuppliedByDevice"
 
-
+# Funzione per raccogliere i dati richiesti dall'utente
 def collect_required_data():
-    # Funzione per raccogliere i dati di configurazione dal'utente
     config = {}
     print("\n\n", "="*80, sep="")
-    print(
-        " "*10, "\033[1m\033[94mThingsBoard device provisioning with basic authorization example script.\033[0m", sep="")
+    print(" "*10, "\033[1m\033[94mEsempio di script per la configurazione di dispositivi ThingsBoard con autorizzazione tramite token di accesso. API MQTT\033[0m", sep="")
     print("="*80, "\n\n", sep="")
     host = input(
-        "Please write your ThingsBoard \033[93mhost\033[0m or leave it blank to use default (thingsboard.cloud): ")
+        "Per favore scrivi il tuo \033[93mhost ThingsBoard\033[0m o lascialo vuoto per usare quello predefinito (thingsboard.cloud): ")
     config["host"] = host if host else "mqtt.thingsboard.cloud"
     port = input(
-        "Please write your ThingsBoard \033[93mport\033[0m or leave it blank to use default (1883): ")
+        "Per favore scrivi la tua \033[93mporta ThingsBoard\033[0m o lasciala vuota per usare quella predefinita (1883): ")
     config["port"] = int(port) if port else 1883
     config["provision_device_key"] = input(
-        "Please write \033[93mprovision device key\033[0m: ")
+        "Per favore scrivi la tua \033[93mchiave di provisioning del dispositivo\033[0m: ")
     config["provision_device_secret"] = input(
-        "Please write \033[93mprovision device secret\033[0m: ")
+        "Per favore scrivi il tuo \033[93msegreto di provisioning del dispositivo\033[0m: ")
+    config["token"] = input(
+        "Per favore scrivi il \033[93mtoken di accesso del dispositivo\033[0m: ")
     device_name = input(
-        "Please write \033[93mdevice name\033[0m or leave it blank to generate: ")
+        "Per favore scrivi il \033[93mnome del dispositivo\033[0m o lascialo vuoto per generarlo: ")
     if device_name:
         config["device_name"] = device_name
     print("\n", "="*80, "\n", sep="")
     return config
 
+# Classe per il client MQTT di provisioning
+
 
 class ProvisionClient(Client):
-    # Sottoclasse di Client per gestire la registrazione del dispositivo su ThingsBoard
-
     PROVISION_REQUEST_TOPIC = "/provision/request"
     PROVISION_RESPONSE_TOPIC = "/provision/response"
 
@@ -56,54 +56,52 @@ class ProvisionClient(Client):
 
     def __on_connect(self, client, userdata, flags, rc):  # Callback per la connessione
         if rc == 0:
-            print("[Provisioning client] Connected to ThingsBoard ")
+            print("[Client di provisioning] Connesso a ThingsBoard ")
             # Iscrizione al topic di risposta del provisioning
             client.subscribe(self.PROVISION_RESPONSE_TOPIC)
             provision_request = dumps(self.__provision_request)
-            print("[Provisioning client] Sending provisioning request %s" %
+            print("[Client di provisioning] Invio della richiesta di provisioning %s" %
                   provision_request)
-            # Invio della richiesta di provisioning
+            # Pubblicazione sul topic di richiesta di provisioning
             client.publish(self.PROVISION_REQUEST_TOPIC, provision_request)
         else:
-            print("[Provisioning client] Cannot connect to ThingsBoard!, result: %s" %
+            print("[Client di provisioning] Impossibile connettersi a ThingsBoard!, risultato: %s" %
                   RESULT_CODES[rc])
 
     def __on_message(self, client, userdata, msg):
-        # Callback per la ricezione di un messaggio
         decoded_payload = msg.payload.decode("UTF-8")
-        print("[Provisioning client] Received data from ThingsBoard: %s" %
+        print("[Client di provisioning] Ricevuti dati da ThingsBoard: %s" %
               decoded_payload)
         decoded_message = loads(decoded_payload)
         provision_device_status = decoded_message.get("status")
         if provision_device_status == "SUCCESS":
             self.__save_credentials(decoded_message["credentialsValue"])
         else:
-            print("[Provisioning client] Provisioning was unsuccessful with status %s and message: %s" % (
+            print("[Client di provisioning] Il provisioning non è riuscito con stato %s e messaggio: %s" % (
                 provision_device_status, decoded_message["errorMsg"]))
         self.disconnect()
 
     def provision(self):
-        # Metodo per gestire la connessione e la richiesta di provisioning
-        print("[Provisioning client] Connecting to ThingsBoard (provisioning client)")
+        print(
+            "[Client di provisioning] Connessione a ThingsBoard (client di provisioning)")
         self.__clean_credentials()
         self.connect(self._host, self._port, 60)
         self.loop_forever()
 
     def get_new_client(self):
-        # Metodo per ottenere un nuovo client configurato con le credenziali di registrazione
         client_credentials = self.__get_credentials()
         new_client = None
         if client_credentials:
             new_client = Client()
             new_client.username_pw_set(client_credentials)
-            print("[Provisioning client] Read credentials from file.")
+            print("[Client di provisioning] Lettura delle credenziali dal file.")
         else:
-            print("[Provisioning client] Cannot read credentials from file!")
+            print(
+                "[Client di provisioning] Impossibile leggere le credenziali dal file!")
         return new_client
 
     @staticmethod
     def __get_credentials():
-        # Metodo statico per ottenere le credenziali dal file
         new_credentials = None
         try:
             with open(FILE_CREDENTIALS, "r") as credentials_file:
@@ -114,57 +112,53 @@ class ProvisionClient(Client):
 
     @staticmethod
     def __save_credentials(credentials):
-        # Metodo statico per salvare le credenziali su un file
         with open(FILE_CREDENTIALS, "w") as credentials_file:
             credentials_file.write(credentials)
 
     @staticmethod
     def __clean_credentials():
-        # Metodo statico per cancellare le credenziali salvate
         open(FILE_CREDENTIALS, "w").close()
 
+# Callback per la connessione con le credenziali ricevute
 
-# Callback per la connessione al servizio ThingsBoard
+
 def on_tb_connected(client, userdata, flags, rc):
     if rc == 0:
-        print("[ThingsBoard client] Connected to ThingsBoard with credentials: %s" %
+        print("[Client ThingsBoard] Connesso a ThingsBoard con le credenziali: %s" %
               client._username.decode())
     else:
-        print("[ThingsBoard client] Cannot connect to ThingsBoard!, result: %s" %
+        print("[Client ThingsBoard] Impossibile connettersi a ThingsBoard!, risultato: %s" %
               RESULT_CODES[rc])
 
 
-# Blocco principale del codice
+# Parte principale del programma
 if __name__ == '__main__':
 
-    # Raccoglie i dati di configurazione
     config = collect_required_data()
 
-    # Configurazione dell'host e della porta di ThingsBoard
-    THINGSBOARD_HOST = config["host"]
-    THINGSBOARD_PORT = config["port"]
+    THINGSBOARD_HOST = config["host"]  # Host dell'istanza ThingsBoard
+    THINGSBOARD_PORT = config["port"]  # Porta MQTT dell'istanza ThingsBoard
 
-    # Creazione della richiesta di provisioning
-    PROVISION_REQUEST = {"provisionDeviceKey": config["provision_device_key"],
+    PROVISION_REQUEST = {"provisionDeviceKey": config["provision_device_key"],  # Chiave di provisioning del dispositivo, sostituire con il valore dal profilo del dispositivo.
+                         # Segreto di provisioning del dispositivo, sostituire con il valore dal profilo del dispositivo.
                          "provisionDeviceSecret": config["provision_device_secret"],
+                         "credentialsType": "ACCESS_TOKEN",
+                         "token": config["token"],
                          }
-
-    # Aggiunge il nome del dispositivo alla richiesta, se fornito
     if config.get("device_name") is not None:
         PROVISION_REQUEST["deviceName"] = config["device_name"]
-
-    # Creazione e avvio del client di provisioning
     provision_client = ProvisionClient(
         THINGSBOARD_HOST, THINGSBOARD_PORT, PROVISION_REQUEST)
+
+    # Richiesta dei dati provisionati
     provision_client.provision()
 
-    # Ottenimento di un nuovo client con le credenziali appena registrate
+    # Ottenimento del client con i dati provisionati
     tb_client = provision_client.get_new_client()
-
-    # Connessione del nuovo client a ThingsBoard
     if tb_client:
+        # Impostazione della callback per la connessione
         tb_client.on_connect = on_tb_connected
         tb_client.connect(THINGSBOARD_HOST, THINGSBOARD_PORT, 60)
-        tb_client.loop_forever()
+        tb_client.loop_forever()  # Avvio del loop infinito
     else:
-        print("Client was not created!")
+        print("Il client non è stato creato!")
